5a29ec40302c7d33a79a9a14b19552cb8090fb64
Giancarlo Martino <gianmart83@gmail.com>
2022-10-26 18:15:19 +0200

    Constants defined

---
+++ JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/Variables_Constants/Constants.java
@@ -0,0 +1,16 @@
+public class Constants {
+    public static void main(String[] args) {
+        /**Java provieds a mechanism tha allows you define a variable, initialize it and moreover fix the variable's value
+         * so that it cannot be changed.
+         * SYNTAX
+         * publi static final Type Variable = Constant value;
+         *
+         * example:
+         */
+        public static final double PI = 3.14159;
+
+        //the convention for naming constants is to use all UPPERCASE letters with an underscore symbol between words
+        public static final int DAYS_PER_WEEK = 7;
+        public static final String ADAGIO = "the customer is right!";
+    }
+}
