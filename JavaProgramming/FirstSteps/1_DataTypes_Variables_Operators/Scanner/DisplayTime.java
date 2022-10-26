c1d6cb3785ea77a261c4f81baba0acfcd94d7fed
Giancarlo Martino <gianmart83@gmail.com>
2022-10-26 18:09:17 +0200

    scanner set up to reads input from keyboard defined

--- JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/Scanner/DisplayTime.java
+++ JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/Scanner/DisplayTime.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
+/**
+ * To accept keyboard input we must sets up things for enable the program
+ * */
+
+
+import java.util.Scanner; //first one: gets the SCANNER class from the package(library) java.util
 
 public class DisplayTime {
 	public static void main(String[] args){
-	Scanner input = new Scanner(System.in);
+	Scanner input = new Scanner(System.in); //second one: create a Scanner so the program can accept keyboard input
 
 	System.out.print("Enter an integer for seconds: ");
-	int seconds = input.nextInt();
+	int seconds = input.nextInt();  //reads one integer number from the keyboard.
+									// We can read different input (explore the class method!)
 
 	int minutes = seconds/60; 
 	int remainingSeconds = seconds % 60;
