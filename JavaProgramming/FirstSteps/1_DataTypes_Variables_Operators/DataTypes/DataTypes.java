85e16262868804c32ccdc913e99f121afb44a01d
Giancarlo Martino <gianmart83@gmail.com>
2022-10-26 17:39:53 +0200

    dataTypes and Variables defined

--- JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/DataTypes/DataTypes.java
+++ JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/DataTypes/DataTypes.java
@@ -1,5 +1,23 @@
 public class DataTypes {
     public static void main(String[] args) {
-        //
+        /**
+        a data type specifies a set of values, and possible operations on these values.
+        Java has two main kinds of data types: class types and primitive types.
+        Class type is a data type for objects of a class.
+         Primitive type are simpler than objects which have both data and methods.
+         A value of a primitive type is an indecomposable value.
+
+         PRIMITIVE TYPES:
+
+         byte (integer) -> 1 byte
+         short (integer)-> 2 bytes
+         int (integer) -> 4 bytes
+         long (integer) -> 8 bytes
+         float(floating-point) -> 4 bytes
+         double(floating-point) -> 8 bytes
+         char (single Unicode character) -> 2 bytes
+         boolean (true or false) -> 1 or more bytes
+
+        */
     }
 }
--- JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/Variables_Constants/Variables.java
+++ JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/Variables_Constants/Variables.java
@@ -1,5 +1,27 @@
 public class Variables {
     public static void main(String[] args) {
-        //
+        /**A variable is a program component used to store or represent data. Can be thought of as containers of a sort.
+        data item stored in a variable is called its VALUE.
+
+        SYNTAX
+        Type Variable1, Variable2, ... ;
+
+        int number, age , deposits;
+        double amount, revenues;
+        char firstNameLetter;
+
+         Technical term for a name in a programming language is IDENTIFIER.
+         Java is case sensitive, so when choose an IDENTIFIER we must consider uppercase and lowercase.
+         A java identifier can contain only letters, digit 0 through 9m and the underscore character.
+         The first character in an identifier cannot be a digit. No dot or asterisk.
+         There is no limit to the length.
+
+         legal identifiers: numberOfPet, hotCar, TimeOfTheDay
+         illegal identifiers: 4life, goodies.com.bye, DUMB*
+
+         in java there are some reserved words with a special predifined funtion and cannot be used as the names of variables,
+         classes, or methods.
+
+         */
     }
 }
