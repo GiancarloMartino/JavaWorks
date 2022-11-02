b3c6124d3bbd7cd753f453eadb6289ade2244daf
Giancarlo Martino <gianmart83@gmail.com>
2022-11-02 15:14:46 +0100

    first impact with java sintax added

---
+++ JavaProgramming/PrimoImpatto.java
@@ -0,0 +1,51 @@
+int size = 27;
+
+
+String name = "Fido";
+
+
+Dog myDog = new Dog(name, size); 
+
+
+x = size - 5;
+
+
+if (x < 15) myDog.bark(8);
+
+
+while (x > 3) {
+
+   myDog.play();
+
+}
+
+
+
+int[] numList = {2, 4, 6, 8}; 
+
+
+
+System.out.print("Hello"); 
+
+
+
+System.out.print("Dog: " + name); 
+
+
+String num = "8";
+
+
+
+int z = Integer.parseInt(num);
+
+
+
+
+try { 
+  readTheFile("myFile.txt");
+  }
+catch (FileNotFoundException ex) {
+
+System.out.print("File not found."); 
+
+}
