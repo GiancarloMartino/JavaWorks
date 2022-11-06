b7b1cb0e36fad686a201f10845f2800ca2be06a9
Giancarlo Martino <gianmart83@gmail.com>
2022-11-06 16:15:12 +0100

    char boolean data types example added

---
+++ JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/DataTypes/Variables_Constants/CharDemo.java
@@ -0,0 +1,14 @@
+// Demonstrate char data type.
+class CharDemo {
+    public static void main(String[] args) {
+
+        //dati
+        char ch1, ch2;
+        ch1 = 88; // code for X
+        ch2 = 'Y';
+
+        //computazione + output
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
