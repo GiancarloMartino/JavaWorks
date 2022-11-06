d8bfac40a044a001fa6d9fb37acb2d2de0986c33
Giancarlo Martino <gianmart83@gmail.com>
2022-11-06 15:46:47 +0100

    datatypes specifications added

--- JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/DataTypes/DataTypes.java
+++ JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/DataTypes/DataTypes.java
@@ -1,11 +1,37 @@
 public class DataTypes {
     public static void main(String[] args) {
         /**
-        a data type specifies a set of values, and possible operations on these values.
-        Java has two main kinds of data types: class types and primitive types.
-        Class type is a data type for objects of a class.
+         A data type is the most basic and the most common classification of data.
+         It is this through which the compiler gets to know the form or the type of information that will be used throughout the code.
+         So basically data type is a type of information transmitted between the programmer and the compiler where
+         the programmer informs the compiler about what type of data is to be stored
+         and also tells how much space it requires in the memory.
+
+         A data type specifies a set of values, and possible operations on these values.
+         It is important to state at the outset that Java is a strongly typed language.
+         Indeed, part of Java’s safety and robustness comes from this fact.
+         First, every variable has a type, every expression has a type, and every type is strictly defined.
+         Second, all assignments, whether explicit or via parameter passing in method calls, are checked for type compatibility.
+         There are no automatic coercions or conversions of conflicting types as in some languages.
+         The Java compiler checks all expressions and parameters to ensure that the types are compatible.
+         Any type mismatches are errors that must be corrected before the compiler will finish compiling the class.
+
+         Java has two main kinds of data types: class types and primitive types.
+         Class type is a data type for objects of a class.
          Primitive type are simpler than objects which have both data and methods.
          A value of a primitive type is an indecomposable value.
+
+         Java defines eight primitive types of data: byte, short, int, long, char, float, double, and boolean.
+         The primitive types are also commonly referred to as simple types.
+         These can be put in four groups:
+         • Integers This group includes byte, short, int, and long, which are for whole-valued signed numbers.
+         • Floating-point numbers This group includes float and double, which represent numbers with fractional precision.
+         • Characters This group includes char, which represents symbols in a character set, like letters and numbers.
+         • Boolean This group includes boolean, which is a special type for representing true/false values.
+         You can use these types as-is, or to construct arrays or your own class types.
+         Thus, they form the basis for all other types of data that you can create.
+         The primitive types represent single values—not complex objects.
+         Although Java is otherwise completely object-oriented, the primitive types are not.
 
          PRIMITIVE TYPES:
 
@@ -18,6 +44,6 @@ public class DataTypes {
          char (single Unicode character) -> 2 bytes
          boolean (true or false) -> 1 or more bytes
 
-        */
+         */
     }
 }
