96538112d2b72e48abfc2c293260db41d795b42e
Giancarlo Martino <gianmart83@gmail.com>
2022-11-06 15:57:57 +0100

    newProgram on simple Computation with double datatypes added

---
+++ JavaProgramming/FirstSteps/1_DataTypes_Variables_Operators/DataTypes/Variables_Constants/SimpleCircleArea.java
@@ -0,0 +1,15 @@
+// Compute the area of a circle.
+class Area {
+public static void main(String[] args) {
+    double pi, r, a;
+
+    //input
+    r = 10.8;// radius
+    pi = 3.1416; // pi, approximately
+
+    //computation
+    a = pi * r * r; // compute area
+
+    //output
+    System.out.println("Area of circle is " + a); }
+}
